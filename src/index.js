1f3546498d92a42acaba1b6eb8f013f5e5ca2d86
cksdlf856 <cksdlf856@naver.com>
2022-10-10 13:19:48 +0900

    add react start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode> 개발테스트용으로 콘솔이 두번찍힘
+    <App />
+  // </React.StrictMode>
+);
