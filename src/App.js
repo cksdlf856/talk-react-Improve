7bf38153e5c0f7c5575c3e7e15fcd8cf1435aa58
fromchaes <94789560+fromchaes@users.noreply.github.com>
2022-10-12 14:11:19 +0900

    add header-nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
-import Header from "./components/Header";
-import Side from "./components/Side";
+import Header from "./components/Header/Header";
+import Side from "./components/Side/Side";
 
 
 function App() {
